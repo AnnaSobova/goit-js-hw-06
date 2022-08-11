39af7d3b3a71306f8baa96d6bef3a9c1cda3a8aa
Anna Sobova <anna_sobova@ukr.net>
2022-08-11 23:45:00 +0300

    with second task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,32 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const navLinkFirstEl= document.createElement('li')
+navLinkFirstEl.classList.add('item');
+navLinkFirstEl.textContent= 'Potatoes';
+
+const navLinkSecondEl= document.createElement('li')
+navLinkSecondEl.classList.add('item');
+navLinkSecondEl.textContent= 'Mushrooms';
+
+const navLinkThirdEl= document.createElement('li')
+navLinkThirdEl.classList.add('item');
+navLinkThirdEl.textContent= 'Garlic';
+
+const navLinkFourthEl= document.createElement('li')
+navLinkFourthEl.classList.add('item');
+navLinkFourthEl.textContent= 'Tomatos';
+
+const navLinkFifthEl= document.createElement('li')
+navLinkFifthEl.classList.add('item');
+navLinkFifthEl.textContent= 'Herbs';
+
+const navLinkSixthEl= document.createElement('li')
+navLinkSixthEl.classList.add('item');
+navLinkSixthEl.textContent= 'Condiments';
+
+const navEl = document.querySelector ("#ingredients");
+navEl.append(navLinkFirstEl,navLinkSecondEl,navLinkThirdEl,navLinkFourthEl,navLinkFifthEl,navLinkSixthEl);
+
+console.log(navEl)
