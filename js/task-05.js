b315b70e2e7ada71a71ee96e826399b9f4f9cfa0
Anna Sobova <anna_sobova@ukr.net>
2022-08-14 23:03:11 +0300

    task5

---
+++ js/task-05.js
@@ -0,0 +1,16 @@
+
+    const input = document.querySelector('#name-input');
+    const span = document.querySelector('#name-output');
+
+input.addEventListener('input', onInputChange);
+
+
+function onInputChange(a){
+    span.textContent = a.currentTarget.value;
+
+  if (a.currentTarget.value === " "){
+    span.textContent = "Anonymous";
+}
+  
+}
+
