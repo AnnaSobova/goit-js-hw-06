d8566c46c95d451eda784aa5f2d818a9c94e93c5
Anna Sobova <anna_sobova@ukr.net>
2022-08-16 13:45:44 +0300

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const form = document.querySelector('.login-form');
 
+const sendTheForm = event => {
+  event.preventDefault();
+
+  const email = event.target.email.value;
+  const password = event.target.password.value;
+
+  email === '' || password === ''
+    ? alert('Please fill in all the fields!')
+    : console.log({ email, password });
+
+  event.currentTarget.reset();
+};
+
+form.addEventListener('submit', sendTheForm);
