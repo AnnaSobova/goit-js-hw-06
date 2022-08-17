444b55c3c6f4c544a473f2ea942ff87c3ae339c5
Anna Sobova <anna_sobova@ukr.net>
2022-08-17 21:40:43 +0300

    9 task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const divWidget = document.querySelector('.widget');
+const span = document.querySelector('.color');
+const button = document.querySelector('.change-color');
+
+button.addEventListener('click',()=>{
+ const color = getRandomHexColor();
+  divWidget.style.backgroundColor = color;
+  span.textContent = color;
+})
